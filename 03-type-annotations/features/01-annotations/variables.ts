c3e54e8bfbcb06dd53054e04109c65e520aa0dfd
pcsmomo <dongwoo_333@hanmail.net>
2021-12-11 15:44:21 +1100

    19. Annotations Around Functions

--- 03-type-annotations/features/01-annotations/variables.ts
+++ 03-type-annotations/features/01-annotations/variables.ts
@@ -1,4 +1,4 @@
-let apples: number = 5;
+let apples = 5;
 let speed: string = 'fast';
 let hasName: boolean = true;
 
@@ -21,4 +21,9 @@ let car: Car = new Car();
 let point: { x: number; y: number } = {
   x: 10,
   y: 20,
+};
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
 };
