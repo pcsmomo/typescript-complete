ecf9018a554c6dae9efbecbbcab5742068957ee5
pcsmomo <dongwoo_333@hanmail.net>
2021-12-14 21:18:02 +1100

    41. Syntax Around Interfaces

--- 07-interface/features/interfaces.ts
+++ 07-interface/features/interfaces.ts
@@ -1,17 +1,22 @@
+/* Basic Interface */
+interface Vehicle {
+  name: string;
+  year: Date;
+  broken: boolean;
+  summary(): string;
+}
+
 const oldCivic = {
   name: 'civic',
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
-const printVehicle = (vehicle: {
-  name: string;
-  year: number;
-  broken: boolean;
-}): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`broken: ${vehicle.broken}`);
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(vehicle.summary());
 };
 
 printVehicle(oldCivic);
