3cc63803ef40312969ffe3f51f1ebcbf34790bb6
pcsmomo <dongwoo_333@hanmail.net>
2021-12-19 16:16:43 +1100

    94. Issues with Inheritance: (Broken)

--- 10-more-design-patterns/sort/src/index.ts
+++ 10-more-design-patterns/sort/src/index.ts
@@ -4,21 +4,22 @@ import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
 console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter2 = new Sorter(charactersCollection);
-sorter2.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter2 = new Sorter(charactersCollection);
+// sorter2.sort();
+// console.log(charactersCollection.data);
 
-const linkedList = new LinkedList();
-linkedList.add(500);
-linkedList.add(-10);
-linkedList.add(-3);
-linkedList.add(4);
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(4);
 
-const sorter3 = new Sorter(linkedList);
-sorter3.sort();
-linkedList.print();
+// const sorter3 = new Sorter(linkedList);
+// sorter3.sort();
+// linkedList.print();
