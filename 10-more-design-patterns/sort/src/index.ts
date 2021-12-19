2488f73efc553c5fcdfe203949e087e61dba43cc
pcsmomo <dongwoo_333@hanmail.net>
2021-12-19 16:37:51 +1100

    97. Solving All Issues with Abstract Classes

--- 10-more-design-patterns/sort/src/index.ts
+++ 10-more-design-patterns/sort/src/index.ts
@@ -1,25 +1,23 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
+console.log('== NumbersCollection ==');
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 numbersCollection.sort();
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
 console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('Xaayb');
-// const sorter2 = new Sorter(charactersCollection);
-// sorter2.sort();
-// console.log(charactersCollection.data);
+console.log('== CharactersCollection ==');
+const charactersCollection = new CharactersCollection('Xaayb');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-10);
-// linkedList.add(-3);
-// linkedList.add(4);
+console.log('== LinkedList ==');
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
 
-// const sorter3 = new Sorter(linkedList);
-// sorter3.sort();
-// linkedList.print();
+linkedList.sort();
+linkedList.print();
