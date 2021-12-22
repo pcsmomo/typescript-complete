5126bf208e7fbbf33f894a9241ef770ef06ac022
pcsmomo <dongwoo_333@hanmail.net>
2021-12-23 08:04:50 +1100

    123. Updating Reader References

--- 11-reusable-code/stats/src/index.ts
+++ 11-reusable-code/stats/src/index.ts
@@ -1,16 +1,18 @@
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
-reader.read();
-// 10/08/2018,Man United,Leicester,2,1,H,A Marriner
-// 0         ,1         ,2        ,3,4,5,6
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
 
-// console.log(reader.data);
+// Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+// matchReader.matches
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
