ac1803a1c5c5a5e4bfd15530c4aa94ec967bb68c
pcsmomo <dongwoo_333@hanmail.net>
2021-12-23 21:53:59 +1100

    132. Generating HTML Reports

--- 11-reusable-code/stats/src/index.ts
+++ 11-reusable-code/stats/src/index.ts
@@ -1,6 +1,7 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
 
@@ -14,7 +15,8 @@ matchReader.load();
 
 const summary = new Summary(
   new WinsAnalysis('Man United'),
-  new ConsoleReport()
+  // new ConsoleReport()
+  new HtmlReport()
 );
 
 summary.buildAndPrintReport(matchReader.matches);
