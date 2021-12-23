1a6874a953c659082ba78e5880082db9d5d5896c
pcsmomo <dongwoo_333@hanmail.net>
2021-12-23 22:07:01 +1100

    copy stats project to implement static way

---
+++ 11-reusable-code/stats-static/src/MatchData.ts
@@ -0,0 +1,13 @@
+import { MatchResult } from './MatchResult';
+
+export type MatchData = [
+  Date,
+  string,
+  string,
+  number,
+  number,
+  MatchResult,
+  string
+];
+// 10/08/2018,Man United,Leicester,2,1,H,A Marriner
+// 0         ,1         ,2        ,3,4,5,6
