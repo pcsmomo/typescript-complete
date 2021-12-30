9a3597ceefda1db4ec8b0a755a5130257b5877bc
pcsmomo <dongwoo_333@hanmail.net>
2021-12-30 18:28:33 +1100

    154. Successfully Fetching Model Data

--- 13-web-framework/web/src/index.ts
+++ 13-web-framework/web/src/index.ts
@@ -1,13 +1,27 @@
-import axios from 'axios';
+import { User } from './models/User';
+
+const user = new User({ id: 1 });
+
+user.fetch();
+
+console.log(JSON.stringify(user, null, 2));
+setTimeout(() => {
+  console.log(JSON.stringify(user, null, 2));
+}, 1000);
+
+//**************************************/
+// Sample Code 2. axios <---> json-server
+// import axios from 'axios';
 
 // axios.post('http://localhost:3000/users', {
 //   name: 'myname',
 //   age: 20,
 // });
 
-axios.get('http://localhost:3000/users/1');
+// axios.get('http://localhost:3000/users/1');
 
-// Sample Code for event listening and triger
+//**************************************/
+// Sample Code 1. for event listening and triger
 // import { User } from './models/User';
 
 // const user = new User({ name: 'myname', age: 20 });
