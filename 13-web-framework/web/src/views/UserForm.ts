057756b552773b9fda368c32816d55c8c159accb
pcsmomo <dongwoo_333@hanmail.net>
2022-01-02 08:02:55 +1100

    195. Binding Events on Class Name

--- 13-web-framework/web/src/views/UserForm.ts
+++ 13-web-framework/web/src/views/UserForm.ts
@@ -5,27 +5,24 @@ export class UserForm {
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onHeaderHover,
+      'click:.set-age': this.onSetAgeClick,
     };
   }
 
-  onHeaderHover(): void {
-    console.log('H1 was hovered over');
-  }
-
-  onButtonClick(): void {
-    console.log('Hi there');
+  onSetAgeClick(): void {
+    console.log('button was clicked');
   }
 
   template(): string {
     return `
-      <div class="userForm">
+      <div class="user-form">
         <h1>User Form</h1>
         <div> User name: ${this.model.get('name')}</div>
         <div> User age: ${this.model.get('age')}</div>
         <input />
         <button>Click Me</button>
+        <br />
+        <button class="set-age">Set Random Age</button>
       </div>
     `;
   }
