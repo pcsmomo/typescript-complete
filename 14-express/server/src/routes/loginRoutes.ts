247f4961520206ab0a9b3f08cea930240dd7a7e8
pcsmomo <dongwoo_333@hanmail.net>
2022-01-03 12:57:13 +1100

    224. Protecting Routes

--- 14-express/server/src/routes/loginRoutes.ts
+++ 14-express/server/src/routes/loginRoutes.ts
@@ -1,7 +1,19 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
+}
+
+// middleware
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    // return next();
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send('Not permitted');
 }
 
 const router = Router();
@@ -57,6 +69,10 @@ router.get('/', (req: Request, res: Response) => {
 router.get('/logout', (req: Request, res: Response) => {
   req.session = undefined;
   res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged in user');
 });
 
 export { router };
