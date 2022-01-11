674e766699e138416b9d8be924fbdde4b8a485ff
pcsmomo <dongwoo_333@hanmail.net>
2022-01-12 06:56:10 +1100

    252. Automated Validation

--- 16-advanced-express/server/src/controllers/decorators/controller.ts
+++ 16-advanced-express/server/src/controllers/decorators/controller.ts
@@ -5,7 +5,21 @@ import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
 
 function bodyValidators(keys: string): RequestHandler {
-  return function (req: Request, res: Response, next: NextFunction) {};
+  return function (req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send('Invalid request');
+      return;
+    }
+
+    for (let key of keys) {
+      if (key in req.body === false) {
+        res.status(422).send('Invalid request');
+        return;
+      }
+    }
+
+    next();
+  };
 }
 
 export function controller(routePrefix: string) {
@@ -29,8 +43,19 @@ export function controller(routePrefix: string) {
         Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) ||
         [];
 
+      const requiredBodyProps =
+        Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) ||
+        [];
+
+      const validator = bodyValidators(requiredBodyProps);
+
       if (path) {
-        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+        router[method](
+          `${routePrefix}${path}`,
+          ...middlewares,
+          validator,
+          routeHandler
+        );
       }
     });
   };
