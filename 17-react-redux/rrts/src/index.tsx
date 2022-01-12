d057da8bc1c772f35edad77304a2a90f9f0d8e0e
pcsmomo <dongwoo_333@hanmail.net>
2022-01-12 16:44:12 +1100

    262. Handling Component State

--- 17-react-redux/rrts/src/index.tsx
+++ 17-react-redux/rrts/src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 interface AppProps {
@@ -6,7 +6,23 @@ interface AppProps {
 }
 
 const App: React.FC<AppProps> = (props) => {
-  return <div>{props.color}</div>;
+  const [counter, setCounter] = useState(0);
+
+  const onIncrement = (): void => {
+    setCounter((prevCount) => prevCount + 1);
+  };
+
+  const onDecrement = (): void => {
+    setCounter((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={onIncrement}>Increment</button>
+      <button onClick={onDecrement}>Decrement</button>
+      {counter}
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
