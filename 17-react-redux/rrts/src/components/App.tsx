809c769296cf37f20371d5d1bc690522b74b20ec
pcsmomo <dongwoo_333@hanmail.net>
2022-01-13 07:02:11 +1100

    272. Rendering a List

--- 17-react-redux/rrts/src/components/App.tsx
+++ 17-react-redux/rrts/src/components/App.tsx
@@ -1,17 +1,27 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Todo, fetchTodos } from '../actions';
 import { StoreState } from '../reducers';
 
-// interface AppProps {
-//   todos: Todo[];
-//   fetchTodos(): any;
-// }
-
 export const App: React.FC = (): JSX.Element => {
   const dispatch = useDispatch();
 
-  const todos = useSelector<StoreState>((state) => state.todos);
+  const todos = useSelector<StoreState, Todo[]>((state) => state.todos);
 
-  return <div>Hi there</div>;
+  const onButtonClick = (): void => {
+    dispatch(fetchTodos());
+  };
+
+  const renderList = (): JSX.Element[] => {
+    return todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={onButtonClick}>Fetch</button>
+      {renderList()}
+    </div>
+  );
 };
