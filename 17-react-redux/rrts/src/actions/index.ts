fb791579b7b154ea7f1421e92012ddd762c5188c
pcsmomo <dongwoo_333@hanmail.net>
2022-01-13 06:32:32 +1100

    267. Action Types Enum

--- 17-react-redux/rrts/src/actions/index.ts
+++ 17-react-redux/rrts/src/actions/index.ts
@@ -1,15 +1,22 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 // Because we are using thunk, it should return a function
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(url);
+    const response = await axios.get<Todo[]>(url);
 
     dispatch({
-      type: 'FETCH_TODOS',
+      type: ActionTypes.fetchTodos,
       payload: response.data,
     });
   };
