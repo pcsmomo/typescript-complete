6f4a973db0901f10f854a77c6b540eb3c3f694a4
pcsmomo <dongwoo_333@hanmail.net>
2022-01-13 07:56:05 +1100

    277. Wiring up deleteToDo Action

--- 17-react-redux/rrts/src/components/App.tsx
+++ 17-react-redux/rrts/src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Todo, fetchTodos } from '../actions';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
 
 export const App: React.FC = (): JSX.Element => {
@@ -12,9 +12,17 @@ export const App: React.FC = (): JSX.Element => {
     dispatch(fetchTodos());
   };
 
+  const onTodoClick = (id: number): void => {
+    dispatch(deleteTodo(id));
+  };
+
   const renderList = (): JSX.Element[] => {
     return todos.map((todo: Todo) => {
-      return <div key={todo.id}>{todo.title}</div>;
+      return (
+        <div onClick={() => onTodoClick(todo.id)} key={todo.id}>
+          {todo.title}
+        </div>
+      );
     });
   };
 
